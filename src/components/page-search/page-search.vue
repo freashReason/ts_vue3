84a0c3e5ab7d9ed4111fe66edde9757de8d1da36
归海 <1428554352@qq.com>
2024-07-19 16:28:47 +0800

    准备高度封装组件

---
+++ src/components/page-search/page-search.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="search">
+    <!-- 1.输入搜索关键字的表单 -->
+    <el-form :model="searchForm" ref="formRef" label-width="80px" size="large">
+      <el-row :gutter="20"> </el-row>
+    </el-form>
+
+    <!-- 2.重置和搜索的按钮 -->
+    <div class="btns">
+      <el-button icon="Refresh" @click="handleResetClick">重置</el-button>
+      <el-button icon="Search" type="primary" @click="handleQueryClick">查询</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+import type { ElForm } from 'element-plus'
+
+const searchForm = reactive({})
+
+// 重置操作
+const emit = defineEmits(['queryClick', 'resertClick'])
+const formRef = ref<InstanceType<typeof ElForm>>()
+function handleResetClick() {
+  formRef.value?.resetFields()
+  handleQueryClick()
+}
+
+function handleQueryClick() {
+  console.log('handleQueryClick')
+
+  emit('queryClick', searchForm)
+}
+</script>
+
+<style lang="less" scoped>
+.search {
+  background-color: #fff;
+  padding: 20px;
+
+  .el-form-item {
+    padding: 20px 30px;
+    margin-bottom: 0;
+    // --el-text-color-regular: red;
+    // label {
+    //   color: red !important;
+    // }
+  }
+
+  .btns {
+    text-align: right;
+    padding: 0 50px 10px 0;
+
+    .el-button {
+      height: 36px;
+    }
+  }
+}
+</style>
